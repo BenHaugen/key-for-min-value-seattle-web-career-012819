c69055a56f77720bf18de90deb489816ade14afc
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-25 01:33:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,8 @@ def key_for_min_value(name_hash)
   min_value = nil 
 name_hash.each do |key, value|
   if value < min_value || min_value == nil
-    return key 
+    min_value = value 
+    min_key = key 
+  end 
+  
 end
